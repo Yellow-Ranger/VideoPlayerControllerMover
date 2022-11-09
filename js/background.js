c1422397e2261740ec5e3e797b54497416c6ac8c
Yellow-Ranger <anesu96@gmail.com>
2022-11-09 18:50:02 -0500

    loads automatically. Need to reload page if cue video in new tab

--- js/background.js
+++ js/background.js
@@ -1,14 +1,10 @@
 function moveVideoControl() {
   let controlBar;
-  let videoPlayer = document.getElementById("player");
   let controlDiv = document.querySelector(".ytp-chrome-bottom");
   let progressBar = document.querySelector(".ytp-progress-bar-container");
   let newParent = document.getElementById("primary-inner");
   let belowDiv = document.getElementById("below");
-  //newParent.append(...controlBar.childNodes);
-  //controlBar.after(videoPlayer);
-  //   controlBar.before(belowDiv);
-  if (controlDiv.parentNode) {
+  if (controlDiv?.parentNode) {
     controlBar = controlDiv.parentNode.removeChild(controlDiv);
   }
   let newDiv = document.createElement("div");
@@ -17,29 +13,17 @@ function moveVideoControl() {
   newDiv.style.justifyContent = "center";
   newDiv.style.width = "100%";
   newDiv.style.bottom = "inherit";
-  // document.body.style.overflowX = "hidden";
   let ytAppContainer = document.querySelector("ytd-app");
-  console.log(ytAppContainer);
   ytAppContainer.style.overflowX = "hidden";
   newParent.insertBefore(newDiv, belowDiv);
   newDiv.appendChild(controlBar);
-  // newParent.insertBefore(controlBar, belowDiv);
-  // controlBar.style.position = "absolute";
-  // controlBar.style.transform = "translate(50%, 50%)";
-  // controlBar.style.left = "12px";
+
   controlBar.style.width = "100%";
   controlBar.style.background = "rgb(101,116,132)";
-  // controlBar.style.marginLeft = "12px";
   controlBar.style.bottom = "inherit";
   progressBar.style.bottom = "inherit";
   controlBar.style.minWidth = "100%";
-  // controlBar.style.margin = "0 auto";
   controlBar.classList.remove("ytp-chrome-bottom");
-  // belowDiv.style.marginTop = "40px";
-  console.log(videoPlayer);
-  console.log(controlBar);
-  console.log(newParent);
-  console.log(belowDiv);
 }
 
 chrome.action.onClicked.addListener((tab) => {
@@ -49,4 +33,11 @@ chrome.action.onClicked.addListener((tab) => {
       function: moveVideoControl,
     });
   }
+});
+
+chrome.webNavigation.onDOMContentLoaded.addListener((tabId) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tabId.tabId },
+    function: moveVideoControl,
+  });
 });
